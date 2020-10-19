5b9c6adb3cd40d146ed6e080ec2fc1aa32b75d8f
Maxim <gorovec2@mail.ru>
2020-10-19 23:17:00 +0300

    Scripted auto-commit on change (2020-10-19 23:17:00) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/JoinReducer.java
+++ src/main/java/ru/bmstu/atom/JoinReducer.java
@@ -3,7 +3,11 @@ package ru.bmstu.atom;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class JoinReducer extends Reducer<IDKey, String, Text, Text> {
     @Override
-    protected void reduce(IDKey key, Iterable<String> values, Context context)
+    protected void reduce(IDKey key, Iterable<String> values, Context context) throws IOException, InterruptedException {
+
+    }
 }
