049d5d69b6175fc33e91b162a1ba4b7b890cb452
Maxim <gorovec2@mail.ru>
2020-10-19 23:38:21 +0300

    Scripted auto-commit on change (2020-10-19 23:38:21) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/JoinReducer.java
+++ src/main/java/ru/bmstu/atom/JoinReducer.java
@@ -25,6 +25,6 @@ public class JoinReducer extends Reducer<IDKey, String, Text, Text> {
             count++;
             avg /= (float) count;
         }
-        context.write();
+        context.write(new Text(key));
     }
 }
