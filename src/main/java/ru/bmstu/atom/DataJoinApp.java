d2099f8c9f8836d8eca10a5e8cbe30600dae83a1
Maxim <gorovec2@mail.ru>
2020-10-19 16:42:43 +0300

    Scripted auto-commit on change (2020-10-19 16:42:43) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/DataJoinApp.java
+++ src/main/java/ru/bmstu/atom/DataJoinApp.java
@@ -2,9 +2,9 @@ package ru.bmstu.atom;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.mapred.lib.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
--- src/main/java/ru/bmstu/atom/FlightJoinMapper.java
+++ src/main/java/ru/bmstu/atom/FlightJoinMapper.java
@@ -1,5 +1,6 @@
 package ru.bmstu.atom;
 
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
@@ -9,9 +10,9 @@ import org.apache.hadoop.mapred.join.TupleWritable;
 
 import java.io.IOException;
 
-public class FlightJoinMapper extends MapReduceBase implements Mapper<Text, TupleWritable, Text, Text> {
+public class FlightJoinMapper extends MapReduceBase implements Mapper<LongWritable, TupleWritable, Text, Text> {
     @Override
-    public void map(Text key, TupleWritable value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+    public void map(LongWritable key, TupleWritable value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
         Text airportCode = (Text)value.get(10);
         Text delay = (Text)value.get(17);
         output.collect(airportCode, delay);
