a81573415563d2069ab1f4d0bb1804d533b557b8
Maxim <gorovec2@mail.ru>
2020-10-19 21:03:42 +0300

    Scripted auto-commit on change (2020-10-19 21:03:42) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/IdPartitioner.java
+++ src/main/java/ru/bmstu/atom/IdPartitioner.java
@@ -1,4 +1,11 @@
 package ru.bmstu.atom;
 
-public class IdPartitioner {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class IdPartitioner extends Partitioner<IDKey, Text> {
+    @Override
+    public int getPartition(IDKey key, Text value, int numPartitions) {
+        return (key.getAirportId().hashCode() & Integer.MAX_VALUE) % numPartitions;
+    }
 }
