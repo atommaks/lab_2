61bc7dc29f2a2f921ceccf37c1b698f364e4bc41
Maxim <gorovec2@mail.ru>
2020-10-19 16:27:31 +0300

    Scripted auto-commit on change (2020-10-19 16:27:31) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/FlightJoinMapper.java
+++ src/main/java/ru/bmstu/atom/FlightJoinMapper.java
@@ -1,4 +1,17 @@
 package ru.bmstu.atom;
 
-public class FlightJoinMapper {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.join.TupleWritable;
+
+import java.io.IOException;
+
+public class FlightJoinMapper extends MapReduceBase implements Mapper<Text, TupleWritable, Text, Text> {
+    @Override
+    public void map(Text key, TupleWritable value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+        Text 
+    }
 }
