822314c58c0417e3c11e3fbeba23d9dc9ee6da4c
Maxim <gorovec2@mail.ru>
2020-10-19 23:17:37 +0300

    Scripted auto-commit on change (2020-10-19 23:17:37) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/JoinReducer.java
+++ src/main/java/ru/bmstu/atom/JoinReducer.java
@@ -8,6 +8,6 @@ import java.io.IOException;
 public class JoinReducer extends Reducer<IDKey, String, Text, Text> {
     @Override
     protected void reduce(IDKey key, Iterable<String> values, Context context) throws IOException, InterruptedException {
-
+        
     }
 }
