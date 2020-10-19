b20738618c7586453951a268d595490042ba3c4b
Maxim <gorovec2@mail.ru>
2020-10-19 17:27:17 +0300

    Scripted auto-commit on change (2020-10-19 17:27:17) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/AirportJoinMapper.java
+++ src/main/java/ru/bmstu/atom/AirportJoinMapper.java
@@ -10,9 +10,12 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportJoinMapper extends Mapper<LongWritable, TupleWritable, LongWritable, Text> {
+    private static final int AIRPORT_CODE_COLUMN_NUMBER = 0;
+    private static final int AIRPORT_DESCRIPTION_COLUMN_NUMBER = 1;
+
     public void map(LongWritable key, TupleWritable value, OutputCollector<LongWritable, Text> output, Reporter reporter) throws IOException {
-        LongWritable airportCode = (LongWritable) value.get(0);
-        Text airportName = (Text) value.get(1);
+        LongWritable airportCode = (LongWritable) value.get(AIRPORT_CODE_COLUMN_NUMBER);
+        Text airportName = (Text) value.get(AIRPORT_DESCRIPTION_COLUMN_NUMBER);
         output.collect(airportCode, airportName);
     }
 }
