de47f5fc18cf4100b4e27263505651340f61c8e8
Maxim <gorovec2@mail.ru>
2020-10-19 23:56:06 +0300

    Scripted auto-commit on change (2020-10-19 23:56:06) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/FlightJoinMapper.java
+++ src/main/java/ru/bmstu/atom/FlightJoinMapper.java
@@ -14,7 +14,7 @@ public class FlightJoinMapper extends Mapper<LongWritable, Text, IDKey, String> 
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (!key.equals(new LongWritable(0))) {
             String[] columns = value.toString().replaceAll(" ", "").split(",");
-            Integer airportId = Integer.parseInt(columns[AIRPORT_CODE_COLUMN_NUMBER]);
+            Integer airportId = Integer.parseInt(columns[AIRPORT_CODE_COLUMN_NUMBER].replaceAll("\"", ""));
             String delay = columns[DELAY_COLUMN_NUMBER];
             context.write(new IDKey(airportId, true, delay), delay);
         }
