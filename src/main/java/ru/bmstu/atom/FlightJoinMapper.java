0f85be6e503e0c90d51cee68045a90bdfa38ca1e
Maxim <gorovec2@mail.ru>
2020-10-19 20:23:21 +0300

    Scripted auto-commit on change (2020-10-19 20:23:21) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/FlightJoinMapper.java
+++ src/main/java/ru/bmstu/atom/FlightJoinMapper.java
@@ -1,7 +1,7 @@
 package ru.bmstu.atom;
 
-import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.join.TupleWritable;
@@ -9,13 +9,13 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class FlightJoinMapper extends Mapper<LongWritable, TupleWritable, IDKey, FloatWritable> {
+public class FlightJoinMapper extends Mapper<LongWritable, TupleWritable, IDKey, Text> {
     private static final int AIRPORT_CODE_COLUMN_NUMBER = 10;
     private static final int DELAY_COLUMN_NUMBER = 17;
 
-    public void map(LongWritable key, TupleWritable value, OutputCollector<IDKey, FloatWritable> output, Reporter reporter) throws IOException {
+    public void map(LongWritable key, TupleWritable value, OutputCollector<IDKey, Text> output, Reporter reporter) throws IOException {
         IDKey airportCode = (IDKey) value.get(AIRPORT_CODE_COLUMN_NUMBER);
-        FloatWritable delay = (FloatWritable) value.get(DELAY_COLUMN_NUMBER);
+        Text delay = (Text) value.get(DELAY_COLUMN_NUMBER);
         output.collect(airportCode, delay);
     }
 }
