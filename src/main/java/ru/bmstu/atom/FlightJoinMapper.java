8c24fae6f68332b6d72df9c7e12b4186dfeaaca1
Maxim <gorovec2@mail.ru>
2020-10-19 17:15:09 +0300

    Scripted auto-commit on change (2020-10-19 17:15:09) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/FlightJoinMapper.java
+++ src/main/java/ru/bmstu/atom/FlightJoinMapper.java
@@ -3,10 +3,11 @@ package ru.bmstu.atom;
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
-import org.apache.hadoop.mapred.join.TupleWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.join.TupleWritable;
+
 
 import java.io.IOException;
 
