586da2887df61ec54e3c47046b2faef6faf5e3c6
Maxim <gorovec2@mail.ru>
2020-10-19 16:21:18 +0300

    Scripted auto-commit on change (2020-10-19 16:21:18) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/DataJoinApp.java
+++ src/main/java/ru/bmstu/atom/DataJoinApp.java
@@ -2,13 +2,15 @@ package ru.bmstu.atom;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.lib.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
 
 public class DataJoinApp {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         if (args.length != 3) {
             System.err.println("Usage: DataJoinApp <input file_1> <input file_2> <output path>");
             System.exit(-1);
