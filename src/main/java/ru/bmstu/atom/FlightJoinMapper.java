1396f1e6784fa3ed1dbad185032974683d898068
Maxim <gorovec2@mail.ru>
2020-10-19 21:59:53 +0300

    Scripted auto-commit on change (2020-10-19 21:59:53) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/FlightJoinMapper.java
+++ src/main/java/ru/bmstu/atom/FlightJoinMapper.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class FlightJoinMapper extends Mapper<LongWritable, Text, IDKey, Text> {
+public class FlightJoinMapper extends Mapper<LongWritable, Text, IDKey, FloatWritable> {
     private static final int AIRPORT_CODE_COLUMN_NUMBER = 10;
     private static final int DELAY_COLUMN_NUMBER = 18;
 
@@ -16,8 +16,8 @@ public class FlightJoinMapper extends Mapper<LongWritable, Text, IDKey, Text> {
         if (!key.equals(new LongWritable(0))) {
             String[] columns = value.toString().split(",");
             Integer airportId = Integer.parseInt(columns[AIRPORT_CODE_COLUMN_NUMBER]);
-            FloatWritable = new FloatWritable(Float.parseFloat(columns[DELAY_COLUMN_NUMBER]));
-            Context.write(new IDKey(airportId, true, value));
+            FloatWritable delay = new FloatWritable(Float.parseFloat(columns[DELAY_COLUMN_NUMBER]));
+            Context.write(new IDKey(airportId, true, value), delay);
         }
     }
 }
