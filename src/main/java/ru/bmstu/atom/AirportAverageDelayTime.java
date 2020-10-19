59d93be262d34576de1e39646af4d502dc6fbe44
Maxim <gorovec2@mail.ru>
2020-10-19 19:28:14 +0300

    Scripted auto-commit on change (2020-10-19 19:28:14) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/AirportAverageDelayTime.java
+++ src/main/java/ru/bmstu/atom/AirportAverageDelayTime.java
@@ -1,8 +1,9 @@
 package ru.bmstu.atom;
 
 public class AirportAverageDelayTime {
-    public abstract class Partitioner<KEY, VALUE> {
 
+    public abstract class Partitioner<KEY, VALUE> {
+        public abstract int getPartition(KEY key, VALUE value, int numPartitions);
     }
 }
 
