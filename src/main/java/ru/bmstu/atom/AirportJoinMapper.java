a21bf7b13ae06ac0c70a8ee1904b85ec9413d067
Maxim <gorovec2@mail.ru>
2020-10-20 00:44:40 +0300

    Scripted auto-commit on change (2020-10-20 00:44:40) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/AirportJoinMapper.java
+++ src/main/java/ru/bmstu/atom/AirportJoinMapper.java
@@ -8,21 +8,23 @@ import java.io.IOException;
 
 public class AirportJoinMapper extends Mapper<LongWritable, Text, IDKey, Text> {
     private static final int AIRPORT_CODE_COLUMN_NUMBER = 0;
-    private static final int AIRPORT_DESCRIPTION_COLUMN_NUMBER = 1;
+    private static final int AIRPORT_DESCRIPTION_COLUMN_NUMBER_ONE = 1;
+    private static final int AIRPORT_DESCRIPTION_COLUMN_NUMBER_TWO = 2;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (!key.equals(new LongWritable(0))) {
             String[] columns = value.toString().replaceAll(" ", "").split(",");
-            System.out.println();
-            System.out.println("--------------------------------------------");
-            for (int i = 0; i < columns.length; i++) {
-                System.out.println(i + ": " + columns[i]);
-            }
-            System.out.println();
-            System.out.println("--------------------------------------------");
+//            System.out.println();
+//            System.out.println("--------------------------------------------");
+//            for (int i = 0; i < columns.length; i++) {
+//                System.out.println(i + ": " + columns[i]);
+//            }
+//            System.out.println();
+//            System.out.println("--------------------------------------------");
             Integer airportCode = Integer.parseInt(columns[AIRPORT_CODE_COLUMN_NUMBER].replaceAll("\"", ""));
-            String name = columns[AIRPORT_DESCRIPTION_COLUMN_NUMBER];
+            String name = columns[AIRPORT_DESCRIPTION_COLUMN_NUMBER_ONE] + columns[AIRPORT_DESCRIPTION_COLUMN_NUMBER_TWO];
+            name = name.replaceAll("\"", "");
             context.write(new IDKey(airportCode, false, name), new Text(name));
         }
     }
