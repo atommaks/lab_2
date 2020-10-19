f46d2d582347a9d6d24e8e6369846c3a01784b49
Maxim <gorovec2@mail.ru>
2020-10-19 19:45:10 +0300

    Scripted auto-commit on change (2020-10-19 19:45:10) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/Partitioner.java
+++
@@ -1,8 +0,0 @@
-package ru.bmstu.atom;
-
-public class Partitioner<K, V> extends org.apache.hadoop.mapreduce.Partitioner<K, V> {
-    @Override
-    public int getPartition(K key, V value, int numPartitions) {
-        return (key.hashCode() & Integer.MAX_VALUE) % numPartitions;
-    }
-}
