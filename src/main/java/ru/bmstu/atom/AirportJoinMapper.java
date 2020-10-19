39df13c5ae6f5279d668706e479a9c6642f49880
Maxim <gorovec2@mail.ru>
2020-10-19 23:21:41 +0300

    Scripted auto-commit on change (2020-10-19 23:21:41) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/AirportJoinMapper.java
+++ src/main/java/ru/bmstu/atom/AirportJoinMapper.java
@@ -13,7 +13,7 @@ public class AirportJoinMapper extends Mapper<LongWritable, Text, IDKey, String>
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (!key.equals(new LongWritable(0))) {
-            String[] columns = value.toString().split(",");
+            String[] columns = value.toString().replaceAll(" ", "").split(",");
             Integer airportCode = Integer.parseInt(columns[AIRPORT_CODE_COLUMN_NUMBER]);
             String name = columns[AIRPORT_DESCRIPTION_COLUMN_NUMBER];
             context.write(new IDKey(airportCode, false, name), name);
