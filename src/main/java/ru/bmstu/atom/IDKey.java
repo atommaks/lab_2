958220fe67202529b01f9a551bf761411821091f
Maxim <gorovec2@mail.ru>
2020-10-19 20:34:49 +0300

    Scripted auto-commit on change (2020-10-19 20:34:49) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/IDKey.java
+++ src/main/java/ru/bmstu/atom/IDKey.java
@@ -3,15 +3,37 @@ package ru.bmstu.atom;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class IDKey implements WritableComparable<IDKey> {
-    private Integer userId;
+    private Integer airportId;
     private Text data;
 
-    public IDKey(Integer userId, Text data) {
+    public IDKey(Integer airportId, Text data) {
         super();
-        this.userId = userId;
+        this.airportId = airportId;
         this.data = data;
     }
 
-    
+    public Integer getAirportId() {
+        return airport;
+    }
+
+    public Text getData() {
+        return data;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(airportId);
+        out.writeChars(data.toString());
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        airportId = in.readInt();
+        data = new Text(in.readLine());
+    }
 }
