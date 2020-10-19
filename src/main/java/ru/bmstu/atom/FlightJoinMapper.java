56619105295392a389bc4d4060e3107b0a1e3bde
Maxim <gorovec2@mail.ru>
2020-10-19 17:26:11 +0300

    Scripted auto-commit on change (2020-10-19 17:26:11) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/FlightJoinMapper.java
+++ src/main/java/ru/bmstu/atom/FlightJoinMapper.java
@@ -10,9 +10,12 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightJoinMapper extends Mapper<LongWritable, TupleWritable, LongWritable, FloatWritable> {
+    private static final int AIRPORT_CODE_COLUMN_NUMBER = 10;
+    private static final int DELAY_COLUMN_NUMBER = 17;
+
     public void map(LongWritable key, TupleWritable value, OutputCollector<LongWritable, FloatWritable> output, Reporter reporter) throws IOException {
-        LongWritable airportCode = (LongWritable) value.get(10);
-        FloatWritable delay = (FloatWritable) value.get(17);
+        LongWritable airportCode = (LongWritable) value.get(AIRPORT_CODE_COLUMN_NUMBER);
+        FloatWritable delay = (FloatWritable) value.get(DELAY_COLUMN_NUMBER);
         output.collect(airportCode, delay);
     }
 }
