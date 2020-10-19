18645c3736c46aec6abd85e9d11f25456678b35d
Maxim <gorovec2@mail.ru>
2020-10-20 01:02:20 +0300

    Scripted auto-commit on change (2020-10-20 01:02:20) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/JoinReducer.java
+++ src/main/java/ru/bmstu/atom/JoinReducer.java
@@ -13,6 +13,7 @@ public class JoinReducer extends Reducer<IDKey, Text, Text, Text> {
         String name = "";
         if (iter.hasNext()) {
             name = iter.next().toString();
+            System.out.println(name);
         }
         float min = Float.MAX_VALUE, max = (float) 0.0, avg = (float) 0.0;
         long count = 0;
