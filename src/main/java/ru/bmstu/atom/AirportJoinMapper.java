9feef849dede436815ccb302abaabd1232b0a2d4
Maxim <gorovec2@mail.ru>
2020-10-20 00:50:34 +0300

    Scripted auto-commit on change (2020-10-20 00:50:34) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/AirportJoinMapper.java
+++ src/main/java/ru/bmstu/atom/AirportJoinMapper.java
@@ -8,8 +8,6 @@ import java.io.IOException;
 
 public class AirportJoinMapper extends Mapper<LongWritable, Text, IDKey, Text> {
     private static final int AIRPORT_CODE_COLUMN_NUMBER = 0;
-    private static final int AIRPORT_DESCRIPTION_COLUMN_NUMBER_ONE = 1;
-    private static final int AIRPORT_DESCRIPTION_COLUMN_NUMBER_TWO = 2;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
