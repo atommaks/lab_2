343911b2a284e6ab1e588dd153aef5535f313220
Maxim <gorovec2@mail.ru>
2020-10-19 16:51:15 +0300

    Scripted auto-commit on change (2020-10-19 16:51:15) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/AirportJoinMapper.java
+++ src/main/java/ru/bmstu/atom/AirportJoinMapper.java
@@ -1,4 +1,10 @@
 package ru.bmstu.atom;
 
-public class AirportJoinMapper {
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.join.TupleWritable;
+
+public class AirportJoinMapper extends MapReduceBase implements Mapper<LongWritable, TupleWritable, LongWritable, FloatWritable> {
 }
