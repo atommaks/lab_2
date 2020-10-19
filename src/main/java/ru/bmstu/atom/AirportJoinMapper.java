00fbcb9dbf67849a2357fbd55e57260cd0eec7e8
Maxim <gorovec2@mail.ru>
2020-10-19 17:20:52 +0300

    Scripted auto-commit on change (2020-10-19 17:20:52) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/AirportJoinMapper.java
+++ src/main/java/ru/bmstu/atom/AirportJoinMapper.java
@@ -2,16 +2,14 @@ package ru.bmstu.atom;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.join.TupleWritable;
+import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class AirportJoinMapper extends MapReduceBase implements Mapper<LongWritable, TupleWritable, LongWritable, Text> {
-    @Override
+public class AirportJoinMapper extends Mapper<LongWritable, TupleWritable, LongWritable, Text> {
     public void map(LongWritable key, TupleWritable value, OutputCollector<LongWritable, Text> output, Reporter reporter) throws IOException {
         LongWritable airportCode = (LongWritable) value.get(0);
         Text airportName = (Text) value.get(1);
