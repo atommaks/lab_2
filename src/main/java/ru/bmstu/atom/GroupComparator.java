de95efe4004fff3aea12a0a00a1d2f2539d1c5f8
Maxim <gorovec2@mail.ru>
2020-10-19 23:06:34 +0300

    Scripted auto-commit on change (2020-10-19 23:06:34) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/GroupComparator.java
+++ src/main/java/ru/bmstu/atom/GroupComparator.java
@@ -3,6 +3,7 @@ package ru.bmstu.atom;
 import org.apache.hadoop.io.WritableComparator;
 
 public class GroupComparator extends WritableComparator {
-    @Override
-    protected 
+    public GroupComparator() {
+        super();
+    }
 }
