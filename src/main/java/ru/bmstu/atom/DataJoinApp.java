a1c7ec792ae81e82b1bb28e3d3290f6dd9e2b6a2
Maxim <gorovec2@mail.ru>
2020-10-19 15:20:57 +0300

    Scripted auto-commit on change (2020-10-19 15:20:57) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/DataJoinApp.java
+++ src/main/java/ru/bmstu/atom/DataJoinApp.java
@@ -2,5 +2,11 @@ package ru.bmstu.atom;
 
 public class DataJoinApp {
     public static void main(String[] args) {
+        if (args.length != 3) {
+            System.err.println("Usage: DataJoinApp <input file_1> <input file_2> <output path>");
+            System.exit(-1);
+        }
+
+        
     }
 }
