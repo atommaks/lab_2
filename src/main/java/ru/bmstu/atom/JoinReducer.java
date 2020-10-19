09ebffa3d9df9cd92599eef7b35ac421672950fb
Maxim <gorovec2@mail.ru>
2020-10-20 00:08:25 +0300

    Scripted auto-commit on change (2020-10-20 00:08:25) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/JoinReducer.java
+++ src/main/java/ru/bmstu/atom/JoinReducer.java
@@ -6,15 +6,15 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 import java.util.Iterator;
 
-public class JoinReducer extends Reducer<IDKey, String, Text, Text> {
+public class JoinReducer extends Reducer<IDKey, Text, Text, Text> {
     @Override
-    protected void reduce(IDKey key, Iterable<String> values, Context context) throws IOException, InterruptedException {
-        Iterator<String> iter = values.iterator();
-        String name = iter.next();
+    protected void reduce(IDKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        String name = iter.next().toString();
         float min = Float.MAX_VALUE, max = (float) 0.0, avg = (float) 0.0;
         long count = 0;
         while (iter.hasNext()) {
-            float currentTime = Float.parseFloat(iter.next());
+            float currentTime = Float.parseFloat(iter.next().toString());
             if (currentTime < min) {
                 min = currentTime;
             }
