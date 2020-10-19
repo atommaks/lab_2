397007120d907b2ae5aecb646171234c3e5c70c3
Maxim <gorovec2@mail.ru>
2020-10-19 23:15:58 +0300

    Scripted auto-commit on change (2020-10-19 23:15:58) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/JoinReducer.java
+++ src/main/java/ru/bmstu/atom/JoinReducer.java
@@ -1,6 +1,8 @@
 package ru.bmstu.atom;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class JoinReducer extends Reducer<IDKey, String, > {
+public class JoinReducer extends Reducer<IDKey, String, Text, Text> {
+    
 }
