7ffe9c3cfbc01b3b593a06399afe356f9c14ace3
Maxim <gorovec2@mail.ru>
2020-10-19 18:06:14 +0300

    Scripted auto-commit on change (2020-10-19 18:06:14) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/DataJoinApp.java
+++ src/main/java/ru/bmstu/atom/DataJoinApp.java
@@ -10,6 +10,8 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import java.io.IOException;
 
 public class DataJoinApp {
+    private static final int TASK_NUM = 2;
+
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         if (args.length != 3) {
             System.err.println("Usage: DataJoinApp <input file_1> <input file_2> <output path>");
@@ -17,8 +19,8 @@ public class DataJoinApp {
         }
 
         Job job = Job.getInstance();
-        job.setJarByClass(JoinJob.class);
-        job.setJobName("JoinJob sort");
+        job.setJarByClass(DataJoinApp.class);
+        job.setJobName("DataJoinApp job");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightJoinMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportJoinMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
@@ -28,7 +30,7 @@ public class DataJoinApp {
         job.setMapOutputKeyClass(TextPair.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        job.setNumReduceTasks(2);
+        job.setNumReduceTasks(TASK_NUM);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
