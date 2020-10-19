0181d2df3a54b0454055f226a9a631d87e089e3a
Maxim <gorovec2@mail.ru>
2020-10-19 16:25:18 +0300

    Scripted auto-commit on change (2020-10-19 16:25:18) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/DataJoinApp.java
+++ src/main/java/ru/bmstu/atom/DataJoinApp.java
@@ -19,8 +19,8 @@ public class DataJoinApp {
         Job job = Job.getInstance();
         job.setJarByClass(JoinJob.class);
         job.setJobName("JoinJob sort");
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportJoinMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setPartitionerClass(TextPair.FirstPartitioner.class);
         job.setGroupingComparatorClass(TextPair.FirstComparator.class);
