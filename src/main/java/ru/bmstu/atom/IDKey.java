33ce040a48c1cff8a6d504f4a085aea26c8e6f0f
Maxim <gorovec2@mail.ru>
2020-10-19 20:27:03 +0300

    Scripted auto-commit on change (2020-10-19 20:27:03) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/IDKey.java
+++ src/main/java/ru/bmstu/atom/IDKey.java
@@ -1,6 +1,17 @@
 package ru.bmstu.atom;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 
 public class IDKey implements WritableComparable<IDKey> {
+    private Integer userId;
+    private Text data;
+
+    public IDKey(Integer userId, Text data) {
+        super();
+        this.userId = userId;
+        this.data = data;
+    }
+
+    
 }
