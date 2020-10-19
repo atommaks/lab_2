9cff8a6a427355fda2b418d76e8c1b6d010b285e
Maxim <gorovec2@mail.ru>
2020-10-19 21:41:33 +0300

    Scripted auto-commit on change (2020-10-19 21:41:32) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/FlightJoinMapper.java
+++ src/main/java/ru/bmstu/atom/FlightJoinMapper.java
@@ -9,28 +9,14 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class FlightJoinMapper extends Mapper<LongWritable, TupleWritable, IDKey, Text> {
+public class FlightJoinMapper extends Mapper<LongWritable, Text, IDKey, Text> {
     private static final int AIRPORT_CODE_COLUMN_NUMBER = 10;
-    private static final int DELAY_COLUMN_NUMBER = 17;
+    //private static final int DELAY_COLUMN_NUMBER = 17;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-
-    }
-
-    public void map(LongWritable key, TupleWritable value, OutputCollector<IDKey, Text> output, Reporter reporter) throws IOException {
-        IDKey airportCode = (IDKey) value.get(AIRPORT_CODE_COLUMN_NUMBER);
-        Text delay = (Text) value.get(DELAY_COLUMN_NUMBER);
-        output.collect(airportCode, delay);
-    }
-
-    @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString();
-        Pattern ptr = Pattern.compile(PATTERN);
-        Matcher matcher = ptr.matcher(line);
-        while (matcher.find()) {
-            context.write(new Text(matcher.group().toLowerCase()), new IntWritable(1));
-        }
+        String[] columns = value.toString().split(",");
+        Integer airportId = Integer.parseInt(columns[AIRPORT_CODE_COLUMN_NUMBER]);
+        Context.write();
     }
 }
