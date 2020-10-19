e202377dca65dbd8c09c1729c801122f51458a4f
Maxim <gorovec2@mail.ru>
2020-10-20 01:03:24 +0300

    Scripted auto-commit on change (2020-10-20 01:03:24) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/JoinReducer.java
+++ src/main/java/ru/bmstu/atom/JoinReducer.java
@@ -10,11 +10,8 @@ public class JoinReducer extends Reducer<IDKey, Text, Text, Text> {
     @Override
     protected void reduce(IDKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
-        String name = "";
-        if (iter.hasNext()) {
-            name = iter.next().toString();
-            System.out.println(name);
-        }
+        String name = iter.next().toString();
+            //System.out.println(name);
         float min = Float.MAX_VALUE, max = (float) 0.0, avg = (float) 0.0;
         long count = 0;
         while (iter.hasNext()) {
