c1d804076a15eb7b1eec759258a1e9a8c098d9cc
Maxim <gorovec2@mail.ru>
2020-10-20 01:30:54 +0300

    Scripted auto-commit on change (2020-10-20 01:30:54) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/JoinReducer.java
+++ src/main/java/ru/bmstu/atom/JoinReducer.java
@@ -11,7 +11,7 @@ public class JoinReducer extends Reducer<IDKey, Text, Text, Text> {
     protected void reduce(IDKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         String name = iter.next().toString();
-        System.out.println(name);
+        //System.out.println(name);
         float min = Float.MAX_VALUE, max = (float) 0.0, avg = (float) 0.0;
         long count = 0;
         while (iter.hasNext()) {
@@ -27,16 +27,7 @@ public class JoinReducer extends Reducer<IDKey, Text, Text, Text> {
             avg /= (float) count;
         }
         if (count > 0) {
-            StringBuilder builder = new StringBuilder();
-            builder.append("Name: ");
-            builder.append("\"" + name + "\"");
-            builder.append(", min: ");
-            builder.append(min);
-            builder.append(", max: ");
-            builder.append(max);
-            builder.append(", avg: ");
-            builder.append(avg);
-            //String res = "Name: " + name + ", min: " + min + ", max: " + max + ", avg: " + avg;
+            String res = "Name: " + name + ", min: " + min + ", max: " + max + ", avg: " + avg;
             System.out.println(builder.toString());
             context.write(new Text(key.getAirportId().toString()), new Text(builder.toString()));
         }
