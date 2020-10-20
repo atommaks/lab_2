57a1d7db7faadc413e065afcabaff37c8aee41b7
Maxim <gorovec2@mail.ru>
2020-10-20 12:23:24 +0300

    Scripted auto-commit on change (2020-10-20 12:23:24) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/JoinReducer.java
+++ src/main/java/ru/bmstu/atom/JoinReducer.java
@@ -31,7 +31,7 @@ public class JoinReducer extends Reducer<IDKey, Text, Text, Text> {
             count++;
             avg /= (float) count;
         }
-        if (key.getFlight()) {
+        if (!key.getFlight()) {
             String res = "Name: " + airportName + ", min: " + min + ", max: " + max + ", avg: " + avg;
             System.out.println(res);
             context.write(new Text(key.getAirportId().toString()), new Text(res));
