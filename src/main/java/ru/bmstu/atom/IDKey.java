1a26366a0d7da0988a575da43c0db2b36305dfd9
Maxim <gorovec2@mail.ru>
2020-10-20 11:35:52 +0300

    Scripted auto-commit on change (2020-10-20 11:35:52) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/IDKey.java
+++ src/main/java/ru/bmstu/atom/IDKey.java
@@ -40,7 +40,8 @@ public class IDKey implements WritableComparable<IDKey> {
     public int compareTo(IDKey obj) {
         int c = this.airportId.compareTo(obj.airportId);
         if (c == 0) {
-            return this.isFlight.compareTo(obj.isFlight);
+            return obj.isFlight.compareTo(this.isFlight);
+            //return this.isFlight.compareTo(obj.isFlight);
         }
         return c;
     }
