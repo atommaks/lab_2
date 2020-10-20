d19682629ef68700e88b5610f52a8d1494121930
Maxim <gorovec2@mail.ru>
2020-10-20 12:32:57 +0300

    Scripted auto-commit on change (2020-10-20 12:32:57) by gitwatch.sh

--- src/main/java/ru/bmstu/atom/JoinReducer.java
+++ src/main/java/ru/bmstu/atom/JoinReducer.java
@@ -13,7 +13,6 @@ public class JoinReducer extends Reducer<IDKey, Text, Text, Text> {
         String airportName = iter.next().toString();
         float min = Float.MAX_VALUE, max = (float) 0.0, avg = (float) 0.0;
         long count = 0;
-//        System.out.println(airportName);
         while (iter.hasNext()) {
             float currentTime = Float.parseFloat(iter.next().toString());
             if (currentTime < min) {
