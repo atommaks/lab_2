96f858192c0b0587508dab183787cd14637fe020
Maxim <gorovec2@mail.ru>
2020-10-31 18:26:05 +0300

    Scripted auto-commit on change (2020-10-31 18:26:05) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/StringTools.java
+++ src/main/java/ru/bmstu/lab2/StringTools.java
@@ -1,12 +1,12 @@
 package ru.bmstu.lab2;
 
 public class StringTools {
-    private static final String COMMA_DELIMITER = ",";
+    private static final String DELIMITER = ",";
 
     public StringTools() {}
 
     public static String[] splitWithCommas(String columns) {
-        return columns.split(COMMA_DELIMITER);
+        return columns.split(DELIMITER);
     }
 
     public static String removeQuotes(String str) {
