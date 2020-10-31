b8cfa13487275db745c5714408f70d530fd16dc7
Maxim <gorovec2@mail.ru>
2020-10-31 18:54:46 +0300

    Scripted auto-commit on change (2020-10-31 18:54:46) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/FlightJoinMapper.java
+++ src/main/java/ru/bmstu/lab2/FlightJoinMapper.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class FlightJoinMapper extends Mapper<LongWritable, Text, IDKey, Text> {
     private static final int AIRPORT_CODE_COLUMN_NUMBER = 14;
     private static final int DELAY_COLUMN_NUMBER = 18;
+    private static final boolean FLAG = true;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
@@ -17,7 +18,7 @@ public class FlightJoinMapper extends Mapper<LongWritable, Text, IDKey, Text> {
             Integer airportId = Integer.parseInt(StringTools.removeQuotes(columns[AIRPORT_CODE_COLUMN_NUMBER]));
             String delay = columns[DELAY_COLUMN_NUMBER];
             if (!delay.isEmpty()) {
-                context.write(new IDKey(airportId, true), new Text(delay));
+                context.write(new IDKey(airportId, FLAG), new Text(delay));
             }
         }
     }
