0e4dc1b14fd40faa00c305f614849e7e7a9e2864
Maxim <gorovec2@mail.ru>
2020-11-02 16:36:45 +0300

    Scripted auto-commit on change (2020-11-02 16:36:45) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/FlightApp.java
+++ src/main/java/ru/bmstu/lab3/FlightApp.java
@@ -1,4 +1,19 @@
 package ru.bmstu.lab3;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class FlightApp {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.err.println("Usage: FlightApp <input file_flight_info> <input file_airport_info> <output path>");
+            System.exit(-1);
+        }
+
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> flightInfoRDD = sc.textFile(args[0]);
+        JavaRDD<String> airportInfoRDD = sc.textFile(args[1]);
+    }
 }
