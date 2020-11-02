3f6c8efced87db6404ccfe58806f34a38e78ff98
Maxim <gorovec2@mail.ru>
2020-11-02 17:45:39 +0300

    Scripted auto-commit on change (2020-11-02 17:45:39) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/FlightApp.java
+++ src/main/java/ru/bmstu/lab3/FlightApp.java
@@ -11,7 +11,7 @@ public class FlightApp {
             System.exit(-1);
         }
 
-        
+        PairFunction<>
 
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
