e9ec0c956636f3d362b32778fca526bc88d2beb1
Maxim <gorovec2@mail.ru>
2020-11-02 22:29:18 +0300

    Scripted auto-commit on change (2020-11-02 22:29:18) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/FlightApp.java
+++ src/main/java/ru/bmstu/lab3/FlightApp.java
@@ -6,6 +6,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
@@ -50,7 +51,7 @@ public class FlightApp {
             }
         };
 
-        
+        Function2<>
 
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
