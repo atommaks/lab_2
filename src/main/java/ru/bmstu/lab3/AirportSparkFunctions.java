c41f3ee0ca73effb1aefddc5d41f849d4e8fa49a
Maxim <gorovec2@mail.ru>
2020-11-02 23:48:19 +0300

    Scripted auto-commit on change (2020-11-02 23:48:19) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/AirportSparkFunctions.java
+++ src/main/java/ru/bmstu/lab3/AirportSparkFunctions.java
@@ -1,4 +1,18 @@
 package ru.bmstu.lab3;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
 public class AirportSparkFunctions {
+    PairFunction<String, LongWritable, Text> airportNamesKeyData = new PairFunction<String, LongWritable, Text>() {
+        @Override
+        public Tuple2<LongWritable, Text> call(String line) {
+            String[] columns = StringTools.splitWithCommas(line);
+            LongWritable airportCode = new LongWritable(Integer.parseInt(StringTools.removeQuotes(columns[AIRPORT_CODE_COLUMN_NUMBER])));
+            Text airportName = new Text(StringTools.concatWords(columns, 1, columns.length));
+            return new Tuple2<>(airportCode, airportName);
+        }
+    };
 }
