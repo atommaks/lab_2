fbbeb4c62aca6d75b50d34e117dd371bacd4822a
Maxim <gorovec2@mail.ru>
2020-11-02 21:22:29 +0300

    Scripted auto-commit on change (2020-11-02 21:22:29) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/FlightData.java
+++ src/main/java/ru/bmstu/lab3/FlightData.java
@@ -4,5 +4,5 @@ import java.io.Serializable;
 
 public class FlightData implements Serializable {
     private float delay;
-    private int 
+    private int abortedFlightCount, delayedFlightCount, flightCount;
 }
