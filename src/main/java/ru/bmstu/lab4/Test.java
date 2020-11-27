79b5609a294aeef30a64c031c0df15e8874307a6
Maxim <gorovec2@mail.ru>
2020-11-27 19:01:39 +0300

    Scripted auto-commit on change (2020-11-27 19:01:39) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/Test.java
+++ src/main/java/ru/bmstu/lab4/Test.java
@@ -2,8 +2,24 @@ package ru.bmstu.lab4;
 
 public class Test {
     private String testName;
-    private String exprectedResult;
+    private String expectedResult;
     private Object[] params;
 
-    
+    public Test(String testName, String expectedResult, Object[] params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public Object[] getParams() {
+        return params;
+    }
 }
