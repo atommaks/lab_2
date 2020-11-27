a7b59cf764cd8632e652bad0eb2533700819bc28
Maxim <gorovec2@mail.ru>
2020-11-27 16:22:52 +0300

    Scripted auto-commit on change (2020-11-27 16:22:52) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/TestingApp.java
+++ src/main/java/ru/bmstu/lab4/TestingApp.java
@@ -3,6 +3,7 @@ package ru.bmstu.lab4;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 public class TestingApp {
     public static void main(String[] args) {
@@ -10,5 +11,6 @@ public class TestingApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         Server server = new Server(system);
+        final Flow<>
     }
 }
