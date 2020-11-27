f9cb57c01c3880f3a21ff292cb3653ece5a68b6c
Maxim <gorovec2@mail.ru>
2020-11-27 19:19:39 +0300

    Scripted auto-commit on change (2020-11-27 19:19:39) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/RouteActor.java
+++ src/main/java/ru/bmstu/lab4/RouteActor.java
@@ -2,9 +2,17 @@ package ru.bmstu.lab4;
 
 import akka.actor.AbstractActor;
 
+import static akka.http.javadsl.server.Directives.route;
+
 public class RouteActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return null;
+    }
+
+    public void createRoute() {
+        route (
+                
+        );
     }
 }
