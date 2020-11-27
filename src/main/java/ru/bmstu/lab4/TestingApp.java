bd5822596b2c08b83a3bff6761e1573ec153a37f
Maxim <gorovec2@mail.ru>
2020-11-27 15:54:07 +0300

    Scripted auto-commit on change (2020-11-27 15:54:07) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/TestingApp.java
+++ src/main/java/ru/bmstu/lab4/TestingApp.java
@@ -5,5 +5,6 @@ import akka.actor.ActorSystem;
 public class TestingApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("Testing");
+        
     }
 }
