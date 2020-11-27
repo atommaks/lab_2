2c508dcac008bc0fcdbcc1272b7257f8a313b403
Maxim <gorovec2@mail.ru>
2020-11-27 22:42:55 +0300

    Scripted auto-commit on change (2020-11-27 22:42:55) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/RunActor.java
+++ src/main/java/ru/bmstu/lab4/RunActor.java
@@ -1,10 +1,11 @@
 package ru.bmstu.lab4;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class RunActor extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return 
+        return ReceiveBuilder.create()
     }
 }
