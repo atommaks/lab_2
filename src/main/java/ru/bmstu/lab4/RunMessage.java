1a95940fa48898c7a21f309cb4c865931eb6a1b0
Maxim <gorovec2@mail.ru>
2020-11-27 20:13:57 +0300

    Scripted auto-commit on change (2020-11-27 20:13:57) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/RunMessage.java
+++ src/main/java/ru/bmstu/lab4/RunMessage.java
@@ -1,4 +1,31 @@
 package ru.bmstu.lab4;
 
 public class RunMessage {
+    private String packageId;
+    private String jsScript;
+    private String functionName;
+    private Test[] tests;
+
+    public RunMessage(String packageId, String jsScript, String functionName, Test[] tests) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
+
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public String getJsScript() {
+        return jsScript;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public Test[] getTests() {
+        return tests;
+    }
 }
