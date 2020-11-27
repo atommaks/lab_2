dff2ca455e6622e91b773665914867c8a8cc2d6b
Maxim <gorovec2@mail.ru>
2020-11-27 19:13:33 +0300

    Scripted auto-commit on change (2020-11-27 19:13:33) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/Request.java
+++ src/main/java/ru/bmstu/lab4/Request.java
@@ -3,5 +3,6 @@ package ru.bmstu.lab4;
 public class Request {
     private String packageId;
     private String jsScript;
-    private String functionName
+    private String functionName;
+    
 }
