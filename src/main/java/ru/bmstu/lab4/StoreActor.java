4ba7c1d0d1dabbabfaf396c7625467e9e86d9043
Maxim <gorovec2@mail.ru>
2020-11-28 15:37:44 +0300

    Scripted auto-commit on change (2020-11-28 15:37:44) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/StoreActor.java
+++ src/main/java/ru/bmstu/lab4/StoreActor.java
@@ -3,13 +3,16 @@ package ru.bmstu.lab4;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.HashMap;
+
 public class StoreActor extends AbstractActor {
+    private HashMap<>
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(StoreMessage.class, m -> {
-                    
+
                 })
                 .build();
     }
