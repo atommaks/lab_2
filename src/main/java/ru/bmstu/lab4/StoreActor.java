9e761e529987a83196e3eb10fd8242c63f431e34
Maxim <gorovec2@mail.ru>
2020-11-28 15:40:27 +0300

    Scripted auto-commit on change (2020-11-28 15:40:27) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/StoreActor.java
+++ src/main/java/ru/bmstu/lab4/StoreActor.java
@@ -16,7 +16,7 @@ public class StoreActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(StoreMessage.class, m -> {
-                    storage.put(m.getPackageID(), )
+                    storage.put(m.getPackageID(), m.getResults());
                 })
                 .build();
     }
