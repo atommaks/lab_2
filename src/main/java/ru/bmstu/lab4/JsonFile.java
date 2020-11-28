ea5b760ae8d71e47e38c579b51ef6fe22fed4dd4
Maxim <gorovec2@mail.ru>
2020-11-28 17:48:10 +0300

    Scripted auto-commit on change (2020-11-28 17:48:10) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/JsonFile.java
+++ src/main/java/ru/bmstu/lab4/JsonFile.java
@@ -4,6 +4,43 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class JsonFile {
+    public static class Test {
+        private String testName;
+        private String expectedResult;
+        private Object[] params;
+
+        public Test(String testName, String expectedResult, Object[] params) {
+            this.testName = testName;
+            this.expectedResult = expectedResult;
+            this.params = params;
+        }
+
+        public String getTestName() {
+            return testName;
+        }
+
+        public String getExpectedResult() {
+            return expectedResult;
+        }
+
+        public Object[] getParams() {
+            return params;
+        }
+
+        public void setTestName(String testName) {
+            this.testName = testName;
+        }
+
+        public void setParams(Object[] params) {
+            this.params = params;
+        }
+
+        public void setExpectedResult(String expectedResult) {
+            this.expectedResult = expectedResult;
+        }
+    }
+
+
     private String packageID;
     private String jsScript;
     private String functionName;
