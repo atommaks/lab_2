b651c8241ddae21ba0055ff742d45ed8084b98f5
Maxim <gorovec2@mail.ru>
2020-11-28 13:40:16 +0300

    Scripted auto-commit on change (2020-11-28 13:40:16) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/HttpServer.java
+++ src/main/java/ru/bmstu/lab4/HttpServer.java
@@ -2,6 +2,7 @@ package ru.bmstu.lab4;
 
 import akka.actor.ActorSystem;
 import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.server.Route;
 
 import static akka.http.javadsl.server.Directives.*;
 import static akka.http.javadsl.server.Directives.entity;
