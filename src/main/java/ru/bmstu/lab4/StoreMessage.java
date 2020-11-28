2b23efb0b3bcf107d567e29095a6357a43785168
Maxim <gorovec2@mail.ru>
2020-11-28 15:55:29 +0300

    Scripted auto-commit on change (2020-11-28 15:55:29) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/StoreMessage.java
+++ src/main/java/ru/bmstu/lab4/StoreMessage.java
@@ -2,13 +2,11 @@ package ru.bmstu.lab4;
 
 public class StoreMessage {
     private String packageID;
-    private String functionName;
     private TestResults results;
     private static final String TEST_RESULTS_FORMAT = "PackageID: %s, Results:\n %s";
 
-    public StoreMessage (String packageID, String functionName, TestResults results) {
+    public StoreMessage (String packageID, TestResults results) {
         this.packageID = packageID;
-        this.functionName = functionName;
         this.results = results;
     }
 
