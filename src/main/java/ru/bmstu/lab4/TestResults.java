f8669a2b10c26d1b120e32139eabd45d53c65395
Maxim <gorovec2@mail.ru>
2020-11-28 14:56:58 +0300

    Scripted auto-commit on change (2020-11-28 14:56:58) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/TestResults.java
+++ src/main/java/ru/bmstu/lab4/TestResults.java
@@ -8,26 +8,31 @@ import java.util.HashMap;
 public class TestResults {
     private HashMap<String, Boolean> results;
     private static final String ENGINE_NAME = "nashorn";
-    private static final String SUCCES_TEST_FORMAT = "PackageID: %s, Test: %s passed!";
-    private static final String FAIL_TEST_FORMAT = "PackageID: %s, Test: %s failed! Expected result: %s, result: %s";
+    private static final String SUCCES_TEST_FORMAT = "PackageID: %s, Function Name: %s, Test: %s passed!\n";
+    private static final String FAIL_TEST_FORMAT = "PackageID: %s, Function Name: %s, Test: %s failed! Expected result: %s, result: %s\n";
+    private static final String CODE_COMPILATION_ERROR_FORMAT = "PackageID: %s, FunctionName: %s, Test: %s failed to compile code!\n";
 
     public TestResults() {
         results = new HashMap<>();
     }
 
     public void runTests(Test[] tests, String jsCode, String functionName, String packageID) throws Exception {
-        for (Test test : tests) {
-            ScriptEngine engine = new ScriptEngineManager().getEngineByName(ENGINE_NAME);
-            engine.eval(jsCode);
-            Invocable invocable = (Invocable)engine;
-            String result = invocable.invokeFunction(functionName, test.getParams()).toString();
-            boolean isRight = result.equals(test.getExpectedResult());
-            if (isRight) {
-                System.out.printf(SUCCES_TEST_FORMAT,packageID, test.getTestName());
-            } else {
-                System.out.printf(FAIL_TEST_FORMAT, packageID, test.getTestName(), test.getExpectedResult(), result);
+        try {
+            for (Test test : tests) {
+                ScriptEngine engine = new ScriptEngineManager().getEngineByName(ENGINE_NAME);
+                engine.eval(jsCode);
+                Invocable invocable = (Invocable)engine;
+                String result = invocable.invokeFunction(functionName, test.getParams()).toString();
+                boolean isRight = result.equals(test.getExpectedResult());
+                if (isRight) {
+                    System.out.printf(SUCCES_TEST_FORMAT,packageID, test.getTestName());
+                } else {
+                    System.out.printf(FAIL_TEST_FORMAT, packageID, test.getTestName(), test.getExpectedResult(), result);
+                }
+                results.put(test.getTestName(), isRight);
             }
-            results.put(test.getTestName(), isRight);
+        } catch (Exception e) {
+
         }
     }
 
