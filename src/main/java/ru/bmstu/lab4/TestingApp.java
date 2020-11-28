d3424ab876a660c7480a288032e0a41a5fdb87f2
Maxim <gorovec2@mail.ru>
2020-11-28 16:37:56 +0300

    Scripted auto-commit on change (2020-11-28 16:37:56) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/TestingApp.java
+++ src/main/java/ru/bmstu/lab4/TestingApp.java
@@ -23,7 +23,6 @@ public class TestingApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         HttpServer server = new HttpServer(system);
-
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.getRoute().flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost(IP_ADDRESS, PORT), materializer);
         System.out.printf(START_MSG_FORMAT, IP_ADDRESS, PORT);
