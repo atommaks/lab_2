56960a94043036bb886a6b38edcb3064b71cf5a9
Maxim <gorovec2@mail.ru>
2020-11-28 14:25:26 +0300

    Scripted auto-commit on change (2020-11-28 14:25:26) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/Test.java
+++ src/main/java/ru/bmstu/lab4/Test.java
@@ -28,6 +28,6 @@ public class Test {
     }
 
     public void runTest() throws Exception {
-        ScriptEngine engine = new ScriptEngineManager()
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName(ENGINE_NAME)
     }
 }
