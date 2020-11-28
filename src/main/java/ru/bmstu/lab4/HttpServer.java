8d0971fb3ea02a08d22e17d473166f4dbe2320f8
Maxim <gorovec2@mail.ru>
2020-11-28 16:00:28 +0300

    Scripted auto-commit on change (2020-11-28 16:00:28) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/HttpServer.java
+++ src/main/java/ru/bmstu/lab4/HttpServer.java
@@ -21,7 +21,7 @@ public class HttpServer {
     private static final String RUN_SEGMENT = "run";
     private static final String RESULT_SEGMENT = "result";
     private static final String PACKAGE_ID_PARAMETR = "packageID";
-    private static final Timeout 
+    private static final Timeout TIMEOUT = 
 
     public HttpServer(ActorSystem system) {
         this.system = system;
