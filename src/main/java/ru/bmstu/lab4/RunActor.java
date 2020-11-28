66e780d5a80de39826f63e1135c284158163ec0c
Maxim <gorovec2@mail.ru>
2020-11-28 15:00:02 +0300

    Scripted auto-commit on change (2020-11-28 15:00:02) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/RunActor.java
+++ src/main/java/ru/bmstu/lab4/RunActor.java
@@ -8,7 +8,8 @@ public class RunActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(RunMessage.class, m -> {
-
+                    TestResults results = new TestResults();
+                    results.runTests(m.getTests(), m.getJsScript(), );
                 })
                 .build();
     }
