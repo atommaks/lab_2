3f1a6d90f795769cb4987f4e9d39af6ea3174d75
Maxim <gorovec2@mail.ru>
2020-11-28 14:01:16 +0300

    Scripted auto-commit on change (2020-11-28 14:01:16) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/HttpServer.java
+++ src/main/java/ru/bmstu/lab4/HttpServer.java
@@ -33,7 +33,7 @@ public class HttpServer {
                                                     file.getTests()), ActorRef.noSender());
                                             return complete(StatusCodes.OK, String.format(PACKAGE_TEST_START_FORMAT, file.getPackageId()));
                                         })))),
-                path ("result")
+                path ("result" / id)
         );
     }
 }
