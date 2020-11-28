86db697db199f5deaabb600085261d017d594cf1
Maxim <gorovec2@mail.ru>
2020-11-28 16:16:34 +0300

    Scripted auto-commit on change (2020-11-28 16:16:34) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/RouteActor.java
+++ src/main/java/ru/bmstu/lab4/RouteActor.java
@@ -33,7 +33,7 @@ public class RouteActor extends AbstractActor {
                 .match(StoreMessage.class, msg -> storageActor.tell(msg, self()))
                 .match(ResultMessage.class, msg -> {
                     Future<Object> future = Patterns.ask(storageActor, msg, TIMEOUT);
-                    sender().tell(Await.result(future, ));
+                    sender().tell(Await.result(future, TIMEOUT.duration()));
 
                 })
                 .build();
