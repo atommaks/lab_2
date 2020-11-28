b00af002cf047ce6aa858587b82844fe20d5b0ff
Maxim <gorovec2@mail.ru>
2020-11-28 13:43:31 +0300

    Scripted auto-commit on change (2020-11-28 13:43:31) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/RouteActor.java
+++ src/main/java/ru/bmstu/lab4/RouteActor.java
@@ -4,7 +4,8 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
 public class RouteActor extends AbstractActor {
-    private final static int 
+    private final static int POOL_SIZE = 2;
+    
 
     @Override
     public Receive createReceive() {
