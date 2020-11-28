68e863191a2da98728dd6ffb757568fdf5394fdb
Maxim <gorovec2@mail.ru>
2020-11-28 16:08:03 +0300

    Scripted auto-commit on change (2020-11-28 16:08:03) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/HttpServer.java
+++ src/main/java/ru/bmstu/lab4/HttpServer.java
@@ -60,9 +60,7 @@ public class HttpServer {
                                     } else {
                                         return complete(StatusCodes.OK, NO_PACKAGE_FOUND_MSG);
                                     }
-                                })
-                        )
-                )
+                                })))
         );
     }
 }
