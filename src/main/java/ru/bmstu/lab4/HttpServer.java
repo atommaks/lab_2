b7f3c6c26354d08545a7868f960fc9a2df6d3179
Maxim <gorovec2@mail.ru>
2020-11-28 15:59:55 +0300

    Scripted auto-commit on change (2020-11-28 15:59:55) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/HttpServer.java
+++ src/main/java/ru/bmstu/lab4/HttpServer.java
@@ -20,6 +20,7 @@ public class HttpServer {
     private static final String RUN_SEGMENT = "run";
     private static final String RESULT_SEGMENT = "result";
     private static final String PACKAGE_ID_PARAMETR = "packageID";
+    private static final 
 
     public HttpServer(ActorSystem system) {
         this.system = system;
