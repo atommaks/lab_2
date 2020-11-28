55ebfb13c4114ecf43be9cc1b1727751bd1b088f
Maxim <gorovec2@mail.ru>
2020-11-28 13:46:31 +0300

    Scripted auto-commit on change (2020-11-28 13:46:31) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/RouteActor.java
+++ src/main/java/ru/bmstu/lab4/RouteActor.java
@@ -14,7 +14,7 @@ public class RouteActor extends AbstractActor {
 
     @Override
     public void preStart() {
-        balanceActor = getContext().actorOf(new BalancingPool(POOL_SIZE).props(Props.create()))
+        balanceActor = getContext().actorOf(new BalancingPool(POOL_SIZE).props(Props.create(RunActor)));
     }
 
     @Override
