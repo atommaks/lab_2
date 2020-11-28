7f4630c329c3d9d1864d19a6b6755e28a8a3c394
Maxim <gorovec2@mail.ru>
2020-11-28 14:58:48 +0300

    Scripted auto-commit on change (2020-11-28 14:58:48) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/TestResults.java
+++ src/main/java/ru/bmstu/lab4/TestResults.java
@@ -10,7 +10,7 @@ public class TestResults {
     private static final String ENGINE_NAME = "nashorn";
     private static final String SUCCES_TEST_FORMAT = "PackageID: %s, Function Name: %s, Test: %s passed!\n";
     private static final String FAIL_TEST_FORMAT = "PackageID: %s, Function Name: %s, Test: %s failed! Expected result: %s, result: %s\n";
-    private static final String CODE_COMPILATION_ERROR_FORMAT = "PackageID: %s, FunctionName: %s, Test: %s failed to compile code!\n";
+    private static final String CODE_COMPILATION_ERROR_FORMAT = "PackageID: %s, FunctionName: %s failed to compile code! Message: %s\n";
 
     public TestResults() {
         results = new HashMap<>();
@@ -25,14 +25,14 @@ public class TestResults {
                 String result = invocable.invokeFunction(functionName, test.getParams()).toString();
                 boolean isRight = result.equals(test.getExpectedResult());
                 if (isRight) {
-                    System.out.printf(SUCCES_TEST_FORMAT,packageID, test.getTestName());
+                    System.out.printf(SUCCES_TEST_FORMAT,packageID, functionName, test.getTestName());
                 } else {
-                    System.out.printf(FAIL_TEST_FORMAT, packageID, test.getTestName(), test.getExpectedResult(), result);
+                    System.out.printf(FAIL_TEST_FORMAT, packageID, functionName, test.getTestName(), test.getExpectedResult(), result);
                 }
                 results.put(test.getTestName(), isRight);
             }
         } catch (Exception e) {
-
+            System.out.printf(CODE_COMPILATION_ERROR_FORMAT, packageID, functionName, e.getMessage());
         }
     }
 
