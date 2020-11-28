9b17117f1c838de01a4cfd2c8ab35103384c4d39
Maxim <gorovec2@mail.ru>
2020-11-28 17:54:19 +0300

    Scripted auto-commit on change (2020-11-28 17:54:19) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/Test.java
+++ src/main/java/ru/bmstu/lab4/Test.java
@@ -5,6 +5,8 @@ public class Test {
     private String expectedResult;
     private Object[] params;
 
+    public Test() {};
+
     public Test(String testName, String expectedResult, Object[] params) {
         this.testName = testName;
         this.expectedResult = expectedResult;
