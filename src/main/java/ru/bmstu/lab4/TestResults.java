3aa1157fe9d99b8a6e2170b757e9c3f1cb3d172f
Maxim <gorovec2@mail.ru>
2020-11-28 14:49:10 +0300

    Scripted auto-commit on change (2020-11-28 14:49:10) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/TestResults.java
+++ src/main/java/ru/bmstu/lab4/TestResults.java
@@ -8,18 +8,25 @@ import java.util.HashMap;
 public class TestResults {
     private HashMap<String, Boolean> results;
     private static final String ENGINE_NAME = "nashorn";
+    private static final String SUCCES_TEST_FORMAT = "";
+    private static final String FAIL_TEST_FORMAT = "";
 
     public TestResults() {
         results = new HashMap<>();
     }
 
-    public void runTests(Test[] tests, String jsCode, String functionName) throws Exception {
+    public void runTests(Test[] tests, String jsCode, String functionName, String packageID) throws Exception {
         for (Test test : tests) {
             ScriptEngine engine = new ScriptEngineManager().getEngineByName(ENGINE_NAME);
             engine.eval(jsCode);
             Invocable invocable = (Invocable)engine;
             String result = invocable.invokeFunction(functionName, test.getParams()).toString();
             boolean isRight = result.equals(test.getExpectedResult());
+            if (isRight) {
+                System.out.printf(SUCCES_TEST_FORMAT,);
+            } else {
+                System.out.printf(FAIL_TEST_FORMAT,);
+            }
             results.put(test.getTestName(), isRight);
 
         }
