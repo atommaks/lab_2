9dd06e959af1aeb70018d40aef9e0c974b0d7026
Maxim <gorovec2@mail.ru>
2020-11-28 15:37:01 +0300

    Scripted auto-commit on change (2020-11-28 15:37:01) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/StoreActor.java
+++ src/main/java/ru/bmstu/lab4/StoreActor.java
@@ -1,11 +1,16 @@
 package ru.bmstu.lab4;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class StoreActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match(StoreMessage.class, m -> {
+
+                })
+                .build();
     }
 }
