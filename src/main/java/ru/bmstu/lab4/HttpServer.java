e07256f71725c81ad12ebb1a41cea9cbd22bdaf3
Maxim <gorovec2@mail.ru>
2020-11-28 18:38:41 +0300

    Scripted auto-commit on change (2020-11-28 18:38:41) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/HttpServer.java
+++ src/main/java/ru/bmstu/lab4/HttpServer.java
@@ -20,7 +20,7 @@ import static akka.http.javadsl.server.Directives.entity;
 public class HttpServer {
     private ActorSystem system;
     private ActorRef route;
-    private static final String PACKAGE_TEST_START_FORMAT = "Testing package %s";
+    private static final String PACKAGE_TEST_START_FORMAT = "Testing package %s\n";
     private static final String RUN_SEGMENT = "run";
     private static final String RESULT_SEGMENT = "result";
     private static final String PACKAGE_ID_PARAMETR = "packageID";
