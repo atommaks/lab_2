83d8d8a7c1d119fa8f88e58d7211b28354146b95
Maxim <gorovec2@mail.ru>
2020-11-28 13:48:58 +0300

    Scripted auto-commit on change (2020-11-28 13:48:58) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/HttpServer.java
+++ src/main/java/ru/bmstu/lab4/HttpServer.java
@@ -18,7 +18,7 @@ public class HttpServer {
                         route (
                                 post(() ->
                                         entity(Jackson.unmarshaller(JsonFile.class), file -> {
-
+                                            
                                         })
                                 )
                         )
