5bfb7839b5e1049c21353918d5b1fd7c12443810
Maxim <gorovec2@mail.ru>
2020-11-28 13:36:04 +0300

    Scripted auto-commit on change (2020-11-28 13:36:04) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/RouteActor.java
+++ src/main/java/ru/bmstu/lab4/RouteActor.java
@@ -8,16 +8,12 @@ import static akka.http.javadsl.server.Directives.*;
 public class RouteActor extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return null;
-    }
-
-    public void createRoute() {
         route (
                 path ( "run", () ->
                         route (
                                 post(() ->
                                         entity(Jackson.unmarshaller(JsonFile.class), file -> {
-                                            
+
                                         })
                                 )
                         )
