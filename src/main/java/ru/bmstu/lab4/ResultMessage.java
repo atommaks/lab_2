89d1f595f5b912cd2574c9a1187bdb4b29355af3
Maxim <gorovec2@mail.ru>
2020-11-28 15:50:48 +0300

    Scripted auto-commit on change (2020-11-28 15:50:48) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/ResultMessage.java
+++ src/main/java/ru/bmstu/lab4/ResultMessage.java
@@ -1,5 +1,9 @@
 package ru.bmstu.lab4;
 
 public class ResultMessage {
-    private String packageId;
+    private String packageID;
+
+    public ResultMessage(String packageID) {
+        this.packageID = packageID;
+    }
 }
