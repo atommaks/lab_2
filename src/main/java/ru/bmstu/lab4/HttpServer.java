5c81f4df9c1d83dcb424e1cea9aa49e97e58fa60
Maxim <gorovec2@mail.ru>
2020-11-28 13:37:10 +0300

    Scripted auto-commit on change (2020-11-28 13:37:10) by gitwatch.sh

--- src/main/java/ru/bmstu/lab4/HttpServer.java
+++ src/main/java/ru/bmstu/lab4/HttpServer.java
@@ -1,9 +1,28 @@
 package ru.bmstu.lab4;
 
 import akka.actor.ActorSystem;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+
+import static akka.http.javadsl.server.Directives.*;
+import static akka.http.javadsl.server.Directives.entity;
 
 public class HttpServer {
     public HttpServer(ActorSystem system) {
 
+    }
+
+    public void createRoute() {
+        route (
+                path ( "run", () ->
+                        route (
+                                post(() ->
+                                        entity(Jackson.unmarshaller(JsonFile.class), file -> {
+
+                                        })
+                                )
+                        )
+
+                )
+        )
     }
 }
--- src/main/java/ru/bmstu/lab4/RouteActor.java
+++ src/main/java/ru/bmstu/lab4/RouteActor.java
@@ -2,25 +2,12 @@ package ru.bmstu.lab4;
 
 import akka.actor.AbstractActor;
 import akka.http.javadsl.marshallers.jackson.Jackson;
-import akka.japi.pf.ReceiveBuilder;
 
 import static akka.http.javadsl.server.Directives.*;
 
 public class RouteActor extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return ReceiveBuilder.create()
-                .route (
-                        path ( "run", () ->
-                                route (
-                                        post(() ->
-                                                entity(Jackson.unmarshaller(JsonFile.class), file -> {
-
-                                                })
-                                        )
-                                )
-
-                        )
-                )
+        return null;
     }
 }
