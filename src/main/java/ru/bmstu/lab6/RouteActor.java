07bb17136d88089e875ffde3a486640b874f8247
Maxim <gorovec2@mail.ru>
2020-12-20 02:20:48 +0300

    Scripted auto-commit on change (2020-12-20 02:20:48) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/RouteActor.java
+++ src/main/java/ru/bmstu/lab6/RouteActor.java
@@ -18,6 +18,6 @@ public class RouteActor extends AbstractActor {
         return ReceiveBuilder
                 .create()
                 .match(ServersList.class, msg -> servers = msg.getServers())
-                .match(Server.class, msg -> getSender().tell(servers.get(random.nextInt(servers.size()))))
+                .match(Server.class, msg -> getSender().tell(servers.get(random.nextInt(servers.size())), ))
     }
 }
