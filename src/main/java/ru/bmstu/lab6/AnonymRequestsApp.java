e1ab220d8d1854b1a1311a90372c697c6d920aae
Maxim <gorovec2@mail.ru>
2020-12-20 02:07:17 +0300

    Scripted auto-commit on change (2020-12-20 02:07:17) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/AnonymRequestsApp.java
+++ src/main/java/ru/bmstu/lab6/AnonymRequestsApp.java
@@ -14,6 +14,7 @@ import java.util.logging.Logger;
 
 public class AnonymRequestsApp {
     private static final String PATH_TO_LOG_FILE = "/home/atom/IdeaProjects/lab_2/lab6.log";
+    private static final String HOST = "localhost";
     public final static Logger LOGGER = Logger.getLogger("lab6");
 
     public static void main(String[] args) throws Exception{
