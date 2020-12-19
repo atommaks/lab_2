7904937e1a6de60511fd8473c4a6b88f86facd51
Maxim <gorovec2@mail.ru>
2020-12-20 02:17:18 +0300

    Scripted auto-commit on change (2020-12-20 02:17:17) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/RouteActor.java
+++ src/main/java/ru/bmstu/lab6/RouteActor.java
@@ -5,7 +5,7 @@ import akka.actor.AbstractActor;
 import java.util.List;
 
 public class RouteActor extends AbstractActor {
-    private List<String> 
+    private List<String> servers
     @Override
     public Receive createReceive() {
         return null;
