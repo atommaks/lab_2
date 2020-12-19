660d7dd202559f308387b6bf38eb74337ac59ca5
Maxim <gorovec2@mail.ru>
2020-12-19 22:52:13 +0300

    Scripted auto-commit on change (2020-12-19 22:52:13) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/CacheTestingApp.java
+++ src/main/java/ru/bmstu/lab5/CacheTestingApp.java
@@ -2,18 +2,23 @@ package ru.bmstu.lab5;
 
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import java.util.concurrent.CompletionStage;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
 public class CacheTestingApp {
     private static final String PATH_TO_LOG_FILE = "/home/atom/IdeaProjects/lab_2/lab5.log";
     public final static Logger LOGGER = Logger.getLogger("lab5");
+    private static final String IP = "localhost";
+    private static final int HOST = 1996;
 
     public static void main(String[] args) throws Exception {
         FileHandler fh = new FileHandler(PATH_TO_LOG_FILE);
@@ -23,7 +28,10 @@ public class CacheTestingApp {
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        //final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = ;
-
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow;
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost()
+        )
     }
 }
