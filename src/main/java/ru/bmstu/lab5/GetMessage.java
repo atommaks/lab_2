b2b35065f0ab3cbc631d8bb90504ae8246b819a6
Maxim <gorovec2@mail.ru>
2020-12-19 23:08:08 +0300

    Scripted auto-commit on change (2020-12-19 23:08:08) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/GetMessage.java
+++ src/main/java/ru/bmstu/lab5/GetMessage.java
@@ -1,4 +1,13 @@
 package ru.bmstu.lab5;
 
 public class GetMessage {
+    private String url;
+
+    public GetMessage(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
