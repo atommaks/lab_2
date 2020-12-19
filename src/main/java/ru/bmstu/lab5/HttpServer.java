ff88b4d29afef5801750879b5a8f4f9a0aae6cce
Maxim <gorovec2@mail.ru>
2020-12-19 23:26:04 +0300

    Scripted auto-commit on change (2020-12-19 23:26:04) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/HttpServer.java
+++ src/main/java/ru/bmstu/lab5/HttpServer.java
@@ -6,6 +6,7 @@ import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.Query;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
@@ -15,7 +16,9 @@ public class HttpServer {
                                                                       ActorMaterializer materializer, ActorRef actor) {
         return Flow.of(HttpRequest.class)
                 .map((r) -> {
-                    
+                    Query query = r.getUri().query();
+                    String url = query.get().get();
                 })
+                .mapAsync()
     }
 }
