e59bd547cfd88c973c8206a69b4fc7288db23b8c
Maxim <gorovec2@mail.ru>
2020-12-20 02:15:19 +0300

    Scripted auto-commit on change (2020-12-20 02:15:19) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/Server.java
+++ src/main/java/ru/bmstu/lab6/Server.java
@@ -3,5 +3,11 @@ package ru.bmstu.lab6;
 public class Server {
     private String url;
 
-    
+    public Server(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
