7c8377323a5e526872ee39ce3d5fa5c02f44e166
Maxim <gorovec2@mail.ru>
2020-12-20 02:39:55 +0300

    Scripted auto-commit on change (2020-12-20 02:39:55) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/HttpServer.java
+++ src/main/java/ru/bmstu/lab6/HttpServer.java
@@ -2,11 +2,13 @@ package ru.bmstu.lab6;
 
 import akka.http.javadsl.server.Route;
 
+import static akka.http.javadsl.server.Directives.route;
+
 public class HttpServer {
 
     public HttpServer() {}
 
     public static Route createRoute() {
-        return 
+        return route()
     }
 }
