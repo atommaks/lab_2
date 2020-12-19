5928f4b5caff99f456035a41b073db4ac0592882
Maxim <gorovec2@mail.ru>
2020-12-19 23:23:33 +0300

    Scripted auto-commit on change (2020-12-19 23:23:33) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/HttpServer.java
+++ src/main/java/ru/bmstu/lab5/HttpServer.java
@@ -1,5 +1,18 @@
 package ru.bmstu.lab5;
 
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
 public class HttpServer {
-    
+
+    public static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(Http http, ActorSystem system,
+                                                                      ActorMaterializer materializer, ActorRef actor) {
+
+    }
 }
