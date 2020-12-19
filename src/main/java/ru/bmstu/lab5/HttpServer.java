bf837bdceb8b27967ad1de5d593586ace66a6662
Maxim <gorovec2@mail.ru>
2020-12-19 23:49:31 +0300

    Scripted auto-commit on change (2020-12-19 23:49:31) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/HttpServer.java
+++ src/main/java/ru/bmstu/lab5/HttpServer.java
@@ -26,7 +26,7 @@ public class HttpServer {
     private static final String COUNT_ARG = "count";
     private static final String INFO_MSG_PTR = "%s - %d";
     private static final int MAP_ASYNC = 1;
-    private static final Timeout TIMEOUT = Timeout.create(Duration.ofSeconds(3));
+    private static final Duration TIMEOUT = Duration.ofSeconds(5);
 
 
     public static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(Http http, ActorSystem system,
