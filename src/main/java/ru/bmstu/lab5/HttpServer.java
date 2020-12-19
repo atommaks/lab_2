f9ff341eb8d84da60e40a039b199becb99408005
Maxim <gorovec2@mail.ru>
2020-12-19 23:35:15 +0300

    Scripted auto-commit on change (2020-12-19 23:35:15) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/HttpServer.java
+++ src/main/java/ru/bmstu/lab5/HttpServer.java
@@ -8,8 +8,13 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.model.Query;
 import akka.japi.Pair;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import akka.util.Timeout;
+
+import java.time.Duration;
+import java.util.concurrent.CompletionStage;
 
 import static ru.bmstu.lab5.CacheTestingApp.LOGGER;
 
@@ -18,18 +23,21 @@ public class HttpServer {
     private static final String COUNT_ARG = "count";
     private static final String INFO_MSG_PTR = "%s - %d";
     private static final int MAP_ASYNC = 1;
+    private static final Timeout TIMEOUT = Timeout.create(Duration.ofSeconds(3));
 
 
     public static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(Http http, ActorSystem system,
                                                                       ActorMaterializer materializer, ActorRef actor) {
         return Flow.of(HttpRequest.class)
-                .map((r) -> {
+                .map(r -> {
                     Query query = r.getUri().query();
                     String url = query.get(URL_ARG).get();
                     int count = Integer.parseInt(query.get(COUNT_ARG).get());
                     LOGGER.info(String.format(INFO_MSG_PTR, url, count));
                     return new Pair<String, Integer>(url, count);
                 })
-                .mapAsync(MAP_ASYNC)
+                .mapAsync(MAP_ASYNC, r -> {
+                    CompletionStage<Object> stage = Patterns.ask(actor, new GetMessage(r.first()), Duration.ofSeconds())
+                })
     }
 }
