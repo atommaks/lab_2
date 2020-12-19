dc8c9cac603b64dc8a63c1002ff4c167fe93998a
Maxim <gorovec2@mail.ru>
2020-12-20 02:13:49 +0300

    Scripted auto-commit on change (2020-12-20 02:13:49) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/ServersList.java
+++ src/main/java/ru/bmstu/lab6/ServersList.java
@@ -5,4 +5,11 @@ import java.util.List;
 public class ServersList {
     private final List<String> servers;
 
+    public ServersList(List<String> servers) {
+        this.servers = servers;
+    }
+
+    public List<String> getServers() {
+        return servers;
+    }
 }
