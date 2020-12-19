281c2e8ec72e550938b7cc6e1f7ac9349a29da0a
Maxim <gorovec2@mail.ru>
2020-12-20 02:06:20 +0300

    Scripted auto-commit on change (2020-12-20 02:06:20) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/AnonymRequestsApp.java
+++ src/main/java/ru/bmstu/lab6/AnonymRequestsApp.java
@@ -3,9 +3,12 @@ package ru.bmstu.lab6;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.stream.ActorMaterializer;
 
+import java.util.concurrent.CompletionStage;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
@@ -22,5 +25,11 @@ public class AnonymRequestsApp {
         ActorRef actor = system.actorOf(Props.create(RouteActor.class));
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(HOST, PORT),
+                materializer
+        );
     }
 }
