38a305b3544e6c93ed4f671c172f54b8bc0df371
Maxim <gorovec2@mail.ru>
2020-12-20 02:08:10 +0300

    Scripted auto-commit on change (2020-12-20 02:08:10) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/AnonymRequestsApp.java
+++ src/main/java/ru/bmstu/lab6/AnonymRequestsApp.java
@@ -33,5 +33,9 @@ public class AnonymRequestsApp {
                 ConnectHttp.toHost(HOST, PORT),
                 materializer
         );
+        LOGGER.info("Server online at http://" + HOST + ":" + PORT + "\n Press any button to stop...");
+        System.in.read();
+        binding.thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 }
