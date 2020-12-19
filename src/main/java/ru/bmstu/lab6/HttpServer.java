97f2e51b4649966c6095b20bd1c4f3f0d809a67e
Maxim <gorovec2@mail.ru>
2020-12-20 02:53:33 +0300

    Scripted auto-commit on change (2020-12-20 02:53:33) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/HttpServer.java
+++ src/main/java/ru/bmstu/lab6/HttpServer.java
@@ -33,7 +33,7 @@ public class HttpServer {
 
                             return completeWithFuture(Patterns.ask(actor, new Server(url), TIMEOUT)
                             .thenApply(port -> (String)port)
-                            .thenCompose(port -> ))
+                            .thenCompose(port -> http.singleRequest(HttpRequest.create())))
                         }))));
     }
 }
