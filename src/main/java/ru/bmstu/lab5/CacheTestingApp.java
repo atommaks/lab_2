800e323d2e828b74b8061078ca8c8822540c84d9
Maxim <gorovec2@mail.ru>
2020-12-19 22:49:59 +0300

    Scripted auto-commit on change (2020-12-19 22:49:59) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/CacheTestingApp.java
+++ src/main/java/ru/bmstu/lab5/CacheTestingApp.java
@@ -1,5 +1,13 @@
 package ru.bmstu.lab5;
 
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
@@ -8,6 +16,14 @@ public class CacheTestingApp {
     public final static Logger LOGGER = Logger.getLogger("lab5");
 
     public static void main(String[] args) throws Exception {
+        FileHandler fh = new FileHandler(PATH_TO_LOG_FILE);
+        LOGGER.addHandler(fh);
+
+        LOGGER.info("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        //final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = ;
 
     }
 }
