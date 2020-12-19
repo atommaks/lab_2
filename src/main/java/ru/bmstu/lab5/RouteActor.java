1673b3f0fe46dae545c31db87982d1b73ebbebea
Maxim <gorovec2@mail.ru>
2020-12-19 23:09:17 +0300

    Scripted auto-commit on change (2020-12-19 23:09:17) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/RouteActor.java
+++ src/main/java/ru/bmstu/lab5/RouteActor.java
@@ -2,8 +2,10 @@ package ru.bmstu.lab5;
 
 import akka.actor.AbstractActor;
 
+import java.util.HashMap;
+
 public class RouteActor extends AbstractActor {
-    private final 
+    private final HashMap<String, Integer>
 
     @Override
     public Receive createReceive() {
