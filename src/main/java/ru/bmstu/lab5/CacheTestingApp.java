d835ad5b6bd259425533735310148a9d8aa9f229
Maxim <gorovec2@mail.ru>
2020-12-19 22:46:41 +0300

    Scripted auto-commit on change (2020-12-19 22:46:41) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/CacheTestingApp.java
+++ src/main/java/ru/bmstu/lab5/CacheTestingApp.java
@@ -4,6 +4,8 @@ import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
 public class CacheTestingApp {
+    private static final String PATH_TO_LOG_FILE = "/home/atom/IdeaProjects/lab_2/lab5.log";
+
     public static void main(String[] args) throws Exception {
 
     }
