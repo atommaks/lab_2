d0bad14d43e6447317aeb48bfbafe14669b4b724
Maxim <gorovec2@mail.ru>
2020-12-20 02:49:19 +0300

    Scripted auto-commit on change (2020-12-20 02:49:19) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/HttpServer.java
+++ src/main/java/ru/bmstu/lab6/HttpServer.java
@@ -1,5 +1,6 @@
 package ru.bmstu.lab6;
 
+import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.server.Route;
@@ -11,6 +12,7 @@ public class HttpServer {
     private static final String URL_ARG = "url";
     private static final String COUNT_ARG = "count";
     private Http http;
+    ActorRef actor
 
     public HttpServer(Http http) {
         this.http = http;
