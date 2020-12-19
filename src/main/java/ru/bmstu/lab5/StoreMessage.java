8b8db3eecc1e63475f830b1edce8f046e403fb34
Maxim <gorovec2@mail.ru>
2020-12-19 23:07:06 +0300

    Scripted auto-commit on change (2020-12-19 23:07:06) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/StoreMessage.java
+++ src/main/java/ru/bmstu/lab5/StoreMessage.java
@@ -4,5 +4,16 @@ public class StoreMessage {
     private String url;
     private int time;
 
-    public
+    public StoreMessage(String url, int time) {
+        this.url = url;
+        this.time = time;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getTime() {
+        return time;
+    }
 }
