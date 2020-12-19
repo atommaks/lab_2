ed0547710d0cba0da6ecb6683cbdbb90fda3d615
Maxim <gorovec2@mail.ru>
2020-12-19 22:46:19 +0300

    Scripted auto-commit on change (2020-12-19 22:46:19) by gitwatch.sh

--- src/main/java/ru/bmstu/lab5/CacheTestingApp.java
+++ src/main/java/ru/bmstu/lab5/CacheTestingApp.java
@@ -1,5 +1,8 @@
 package ru.bmstu.lab5;
 
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
 public class CacheTestingApp {
     public static void main(String[] args) throws Exception {
 
