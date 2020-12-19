bd99f4e2506054a1c01640e66641cafd930db80f
Maxim <gorovec2@mail.ru>
2020-12-20 02:40:30 +0300

    Scripted auto-commit on change (2020-12-20 02:40:30) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/HttpServer.java
+++ src/main/java/ru/bmstu/lab6/HttpServer.java
@@ -9,8 +9,6 @@ public class HttpServer {
     public HttpServer() {}
 
     public static Route createRoute() {
-        return route(
-                get((() -> ))
-        )
+        return route()
     }
 }
