a7f0d2c52a2ad4ed50303bbf0c29dae5469c22d6
Maxim <gorovec2@mail.ru>
2020-12-20 16:39:29 +0300

    Scripted auto-commit on change (2020-12-20 16:39:29) by gitwatch.sh

--- src/main/java/ru/bmstu/lab7/Client.java
+++ src/main/java/ru/bmstu/lab7/Client.java
@@ -6,8 +6,10 @@ import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Client {
+    private static final String PATH_TO_LOG_FILE = "/home/atom/IdeaProjects/lab_2/logs/lab6.log";
     private static final String CLIENT_ADDRESS = "tcp://localhost:1969";
     private static final String EXIT_CMD = "quit";
     private static final int TIMEOUT = 5000;
@@ -15,6 +17,7 @@ public class Client {
     private static final Scanner in = new Scanner(System.in);
     private static ZContext context;
     private static ZMQ.Socket client;
+    public final static Logger LOGGER = Logger.getLogger("lab6");
 
     public static void main(String[] args) {
         context = new ZContext(TYPE);
@@ -28,7 +31,7 @@ public class Client {
             client.send(cmd);
             String output = client.recvStr();
             if (output != null) {
-                
+
             }
         }
 
