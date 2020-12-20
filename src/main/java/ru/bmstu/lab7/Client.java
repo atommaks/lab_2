928b5b7642b48a651e0677b93d7f0de0809b86a8
Maxim <gorovec2@mail.ru>
2020-12-20 16:28:54 +0300

    Scripted auto-commit on change (2020-12-20 16:28:53) by gitwatch.sh

--- src/main/java/ru/bmstu/lab7/Client.java
+++ src/main/java/ru/bmstu/lab7/Client.java
@@ -2,11 +2,13 @@ package ru.bmstu.lab7;
 
 
 import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
 
 public class Client {
     private static final String CLIENT_ADDRESS = "tcp://localhost:1969";
     private static final int TIMEOUT = 5000;
     private static ZContext context;
+    private static ZMQ.Socket Client;
 
     public static void main(String[] args) {
 
