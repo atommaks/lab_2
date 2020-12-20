03ee154225196ae219ee33af51c55edddb9b2685
Maxim <gorovec2@mail.ru>
2020-12-20 03:14:34 +0300

    Scripted auto-commit on change (2020-12-20 03:14:34) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/ZooKeeperConn.java
+++ src/main/java/ru/bmstu/lab6/ZooKeeperConn.java
@@ -1,14 +1,24 @@
 package ru.bmstu.lab6;
 
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
+import java.io.IOException;
 import java.time.Duration;
 
 public class ZooKeeperConn {
     private static final String HOST = "localhost:2181";
     private ZooKeeper keeper;
 
-    public ZooKeeperConn() {
-        keeper = new ZooKeeper(HOST, Duration.ofSeconds(5))
+    public static Watcher watcher = watchedEvent -> {
+
+    };
+
+    public ZooKeeperConn() throws IOException, KeeperException, InterruptedException {
+        keeper = new ZooKeeper(HOST, (int)Duration.ofSeconds(5).getSeconds() * 1000, watcher);
+        keeper.create("/servers/" + AnonymRequestsApp.PORT, (AnonymRequestsApp.PORT + "").getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        WatchedEvent event = new WatchedEvent(Watcher.Event.EventType.NodeCreated, Watcher.Event.KeeperState.SyncConnected, "");
+        watcher.process(event);
+
     }
 }
