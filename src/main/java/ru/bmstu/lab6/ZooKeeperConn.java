0e33c888bb10c4410389d60e7e193849f7341f03
Maxim <gorovec2@mail.ru>
2020-12-20 03:18:33 +0300

    Scripted auto-commit on change (2020-12-20 03:18:33) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/ZooKeeperConn.java
+++ src/main/java/ru/bmstu/lab6/ZooKeeperConn.java
@@ -4,16 +4,25 @@ import org.apache.zookeeper.*;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
 
 public class ZooKeeperConn {
     private static final String HOST = "localhost:2181";
-    private ZooKeeper keeper;
+    private static ZooKeeper keeper;
 
     public static Watcher watcher = watchedEvent -> {
         if (watchedEvent.getType() == Watcher.Event.EventType.NodeCreated ||
                 watchedEvent.getType() == Watcher.Event.EventType.NodeDeleted ||
                 watchedEvent.getType() == Watcher.Event.EventType.NodeDataChanged) {
-            
+            List<String> servers = Collections.emptyList();
+            try {
+                for (String s : keeper.getChildren("/servers", null)) {
+
+                }
+            } catch (KeeperException | InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     };
 
