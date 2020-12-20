f9e2856569311304db881b5df5ac5d2943f1e405
Maxim <gorovec2@mail.ru>
2020-12-20 16:13:55 +0300

    Scripted auto-commit on change (2020-12-20 16:13:55) by gitwatch.sh

--- src/main/java/ru/bmstu/lab7/Client.java
+++ src/main/java/ru/bmstu/lab7/Client.java
@@ -2,6 +2,7 @@ package ru.bmstu.lab7;
 
 public class Client {
     private static final String CLIENT_ADDRESS = "tcp://localhost:1969";
+    private static final int TIMEOUT = 5000;
     
 
     public static void main(String[] args) {
