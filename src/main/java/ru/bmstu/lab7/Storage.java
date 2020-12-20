947c58438554f516af1b6f87f851abb024331cb7
Maxim <gorovec2@mail.ru>
2020-12-20 17:13:20 +0300

    Scripted auto-commit on change (2020-12-20 17:13:20) by gitwatch.sh

--- src/main/java/ru/bmstu/lab7/Storage.java
+++ src/main/java/ru/bmstu/lab7/Storage.java
@@ -4,6 +4,8 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+import static ru.bmstu.lab7.Server.SERVER_ADDRESS;
+
 public class Storage {
     private static final int TYPE = 1;
 
@@ -11,6 +13,6 @@ public class Storage {
     public static void main(String[] args) {
         ZContext context = new ZContext(TYPE);
         ZMQ.Socket socket = context.createSocket(SocketType.DEALER);
-
+        
     }
 }
