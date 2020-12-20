9ec620387ccaa6c0e2b4925c7bc4b132a61b77da
Maxim <gorovec2@mail.ru>
2020-12-20 17:12:52 +0300

    Scripted auto-commit on change (2020-12-20 17:12:52) by gitwatch.sh

--- src/main/java/ru/bmstu/lab7/Server.java
+++ src/main/java/ru/bmstu/lab7/Server.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 public class Server {
     private static final String PATH_TO_LOG_FILE = "/home/atom/IdeaProjects/lab_2/logs/lab7.log";
     public final static Logger LOGGER = Logger.getLogger("lab6");
+    public final static String SERVER_ADDRESS = "tcp://localhost:1970";
 
     public static void main(String[] args) throws Exception{
         BasicConfigurator.configure();
