44ed7419b8dfb7eb1da8a1bbdf6e42bb6c19288d
Maxim <gorovec2@mail.ru>
2020-12-20 03:06:36 +0300

    Scripted auto-commit on change (2020-12-20 03:06:36) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/ZooKeeperConn.java
+++ src/main/java/ru/bmstu/lab6/ZooKeeperConn.java
@@ -1,5 +1,9 @@
 package ru.bmstu.lab6;
 
 public class ZooKeeperConn {
-    private static final String HOST = ""
+    private static final String HOST = "localhost:2181";
+
+    public ZooKeeperConn() {
+        
+    }
 }
