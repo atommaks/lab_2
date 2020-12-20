b0371165db3eb64926b61c54eb0bfb660fdc96e0
Maxim <gorovec2@mail.ru>
2020-12-20 17:22:57 +0300

    Scripted auto-commit on change (2020-12-20 17:22:57) by gitwatch.sh

--- src/main/java/ru/bmstu/lab7/Storage.java
+++ src/main/java/ru/bmstu/lab7/Storage.java
@@ -14,7 +14,7 @@ public class Storage {
     private static final int TYPE = 1;
     private static final int POLLER_SIZE = 1;
     private final static long NOTICE_TIME = 1000;
-    private final static String NOTICE_MSG_PTR = "notice";
+    private final static String NOTICE_MSG_PTR = "notice id:%s";
     private static final int ZERO_POLL_INDEX = 0;
 
 
