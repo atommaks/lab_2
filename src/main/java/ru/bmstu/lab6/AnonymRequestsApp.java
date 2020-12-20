bba05435ec279562cb489d38ecc2eace185c3b6f
Maxim <gorovec2@mail.ru>
2020-12-20 15:25:26 +0300

    Scripted auto-commit on change (2020-12-20 15:25:26) by gitwatch.sh

--- src/main/java/ru/bmstu/lab6/AnonymRequestsApp.java
+++ src/main/java/ru/bmstu/lab6/AnonymRequestsApp.java
@@ -21,7 +21,7 @@ import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
 public class AnonymRequestsApp {
-    private static final String PATH_TO_LOG_FILE = "/home/atom/IdeaProjects/lab_2/lab6.log";
+    private static final String PATH_TO_LOG_FILE = "/home/atom/IdeaProjects/lab_2/logs/lab6.log";
     private static final String ZOOKEEPER_HOST = "127.0.0.1:2181";
     public static final String HOST = "localhost";
     public static int PORT;
