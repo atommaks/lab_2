354caa92f5e23848b9df6a94d6f9889833c6d530
Maxim <gorovec2@mail.ru>
2020-12-20 16:35:32 +0300

    Scripted auto-commit on change (2020-12-20 16:35:32) by gitwatch.sh

--- src/main/java/ru/bmstu/lab7/Client.java
+++ src/main/java/ru/bmstu/lab7/Client.java
@@ -5,17 +5,20 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+import java.util.Scanner;
+
 public class Client {
     private static final String CLIENT_ADDRESS = "tcp://localhost:1969";
     private static final int TIMEOUT = 5000;
     private static final int TYPE = 1;
+    private static final Scanner in = new Scanner(System.in);
     private static ZContext context;
     private static ZMQ.Socket client;
 
     public static void main(String[] args) {
         context = new ZContext(TYPE);
         createAndConnectSocket();
-        
+
 
     }
 
