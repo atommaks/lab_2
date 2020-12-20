1d428c40bcf269a0d97b565029ff7302ef78a8bc
Maxim <gorovec2@mail.ru>
2020-12-20 17:14:07 +0300

    Scripted auto-commit on change (2020-12-20 17:14:07) by gitwatch.sh

--- src/main/java/ru/bmstu/lab7/Storage.java
+++ src/main/java/ru/bmstu/lab7/Storage.java
@@ -8,12 +8,14 @@ import static ru.bmstu.lab7.Server.SERVER_ADDRESS;
 
 public class Storage {
     private static final int TYPE = 1;
+    private static final int POLLER_SIZE = 1;
 
 
     public static void main(String[] args) {
         ZContext context = new ZContext(TYPE);
         ZMQ.Socket socket = context.createSocket(SocketType.DEALER);
         socket.connect(SERVER_ADDRESS);
-        ZMQ.Poller poller = 
+        ZMQ.Poller poller = context.createPoller(POLLER_SIZE);
+        
     }
 }
