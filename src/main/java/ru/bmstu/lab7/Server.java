3c1d35aedaa23e8171bbddff44074937a27a4a8a
Maxim <gorovec2@mail.ru>
2020-12-20 16:46:17 +0300

    Scripted auto-commit on change (2020-12-20 16:46:17) by gitwatch.sh

--- src/main/java/ru/bmstu/lab7/Server.java
+++ src/main/java/ru/bmstu/lab7/Server.java
@@ -1,17 +1,17 @@
-package ru.bmstu.lab7;
-
-import org.apache.log4j.BasicConfigurator;
-
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-
-public class Server {
-    private static final String PATH_TO_LOG_FILE = "/home/atom/IdeaProjects/lab_2/logs/lab7.log";
-    public final static Logger LOGGER = Logger.getLogger("lab6");
-
-    public static void main(String[] args) throws Exception{
-        BasicConfigurator.configure();
-        FileHandler fh = new FileHandler(PATH_TO_LOG_FILE);
-        LOGGER.addHandler(fh);
-    }
-}
+//package ru.bmstu.lab7;
+//
+//import org.apache.log4j.BasicConfigurator;
+//
+//import java.util.logging.FileHandler;
+//import java.util.logging.Logger;
+//
+//public class Server {
+//    private static final String PATH_TO_LOG_FILE = "/home/atom/IdeaProjects/lab_2/logs/lab7.log";
+//    public final static Logger LOGGER = Logger.getLogger("lab6");
+//
+//    public static void main(String[] args) throws Exception{
+//        BasicConfigurator.configure();
+//        FileHandler fh = new FileHandler(PATH_TO_LOG_FILE);
+//        LOGGER.addHandler(fh);
+//    }
+//}
