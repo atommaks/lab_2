792437ce40879bfcff3779acf15ba5d2b3242008
Maxim <gorovec2@mail.ru>
2020-12-20 17:10:34 +0300

    Scripted auto-commit on change (2020-12-20 17:10:34) by gitwatch.sh

--- src/main/java/ru/bmstu/lab7/Storage.java
+++ src/main/java/ru/bmstu/lab7/Storage.java
@@ -1,8 +1,11 @@
 package ru.bmstu.lab7;
 
+import org.zeromq.ZContext;
+
 public class Storage {
+    
 
     public static void main(String[] args) {
-
+        ZContext context = new ZContext()
     }
 }
