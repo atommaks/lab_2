98179ac1e875c27015efaff2fd45bb0775c97368
Maxim <gorovec2@mail.ru>
2020-12-20 16:42:08 +0300

    Scripted auto-commit on change (2020-12-20 16:42:08) by gitwatch.sh

--- src/main/java/ru/bmstu/lab7/Server.java
+++ src/main/java/ru/bmstu/lab7/Server.java
@@ -1,8 +1,14 @@
 package ru.bmstu.lab7;
 
+import org.apache.log4j.BasicConfigurator;
+
 import java.util.logging.Logger;
 
 public class Server {
     private static final String PATH_TO_LOG_FILE = "/home/atom/IdeaProjects/lab_2/logs/lab7.log";
     public final static Logger LOGGER = Logger.getLogger("lab6");
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+    }
 }
