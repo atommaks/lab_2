a412879e850c065d7cd4c6bca49b976639aaad14
Maxim <gorovec2@mail.ru>
2020-12-20 16:48:35 +0300

    Scripted auto-commit on change (2020-12-20 16:48:35) by gitwatch.sh

--- src/main/java/ru/bmstu/lab7/Server.java
+++ src/main/java/ru/bmstu/lab7/Server.java
@@ -9,7 +9,7 @@ public class Server {
     private static final String PATH_TO_LOG_FILE = "/home/atom/IdeaProjects/lab_2/logs/lab7.log";
     public final static Logger LOGGER = Logger.getLogger("lab6");
 
-    public static void main(String[] args) throws Exception{
+    public static void lol(String[] args) throws Exception{
         BasicConfigurator.configure();
         FileHandler fh = new FileHandler(PATH_TO_LOG_FILE);
         LOGGER.addHandler(fh);
