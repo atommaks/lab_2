e5d4dd4580b8c5007af0f0ed63ccb49d73340a89
Maxim <gorovec2@mail.ru>
2020-12-20 17:54:52 +0300

    Scripted auto-commit on change (2020-12-20 17:54:51) by gitwatch.sh

--- src/main/java/ru/bmstu/lab7/Storage.java
+++ src/main/java/ru/bmstu/lab7/Storage.java
@@ -63,7 +63,7 @@ public class Storage {
         String[] split = msg.split(DELIMITER);
         long key = Integer.parseInt(split[1]);
         String value = split[2];
-        
+        caches.set()
     }
 
     private static void executeGetCmd(ZMsg msg, String message, ArrayList<String> caches, long start) {
